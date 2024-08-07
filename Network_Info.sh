7231bcc252ac01309996dc2caff843b9f97b111e
MohamedElSayed215 <mohamedelsayedmahmoud21@gmail.com>
2024-08-07 20:12:51 +0300

    Update Network_Info.sh

--- Network_Info.sh
+++ Network_Info.sh
@@ -2,12 +2,11 @@
 Network_Info()
 {
 echo "IP of the Network is : $(hostname | xargs -n 1 dig +short A)"
-
+sleep 2                    
 echo "DNS Information  :  $( cat /etc/resolv.conf | grep -i nameserver )"
-
+sleep 2
 speedtest
 
 }
 Network_Info
 echo "$(date) : $0" >> Kernel_logs.txt 
-

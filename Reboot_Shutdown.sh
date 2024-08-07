8d1feebd5093efebcf41e89007486a4a651923e3
MohamedElSayed215 <mohamedelsayedmahmoud21@gmail.com>
2024-08-07 20:13:45 +0300

    Update Reboot_Shutdown.sh

--- Reboot_Shutdown.sh
+++ Reboot_Shutdown.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+Reboot_Shutdown(){
 echo "$(date) : $0" >> Kernel_logs.txt
 while [ true ] ; 
 do
 
-    select choice in Reboot Shutdown
+    select choice in Reboot Shutdown "EXIT PAGE"
     do
         case $choice in 
         Reboot)
@@ -13,7 +13,15 @@ do
         Shutdown)
         sudo shutdown now 
         ;;
+        "EXIT PAGE")
+        return 0 
+        ;; 
+         
         esac 
         break 
         done
 done
+}
+
+
+Reboot_Shutdown
